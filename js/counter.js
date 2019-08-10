d937eda5ad9b0c20d35962d0e274f08d0f9a63c7
XuTianWen <TianWen.Xu@protonmail.com>
2019-08-10 20:30:34 +0800

    Site updated: 2019-08-10 20:30:33

---
+++ js/counter.js
@@ -0,0 +1,21 @@
+
+//BaiduCounter
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4740160f2157a11f3aa4e0d430d7f8b8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+//CNZZCounter
+
+document.writeln('<script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1277909986&web_id=1277909986"></script>');
+
+//	var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+//	document.write(unescape("%3Cspan id='cnzz_stat_icon_1277909986'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1277909986' type='text/javascript'%3E%3C/script%3E"));
+
+//51.laCounter
+
+document.writeln('<script type="text/javascript"  src="https://js.users.51.la/20261703.js"></script>');
